2821d414e649898404229b9a801389bf3096c827
seawinda <lekochka@gmail.com>
2019-11-15 16:37:25 +0300

    Updates

---
+++ precache-manifest.a515aa45750ac89d58df2a02b28a54d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb564c9c7d74b8d5ce6a186e26d15f71",
+    "url": "/test_prime/index.html"
+  },
+  {
+    "revision": "8267d49f908edf88bb5c",
+    "url": "/test_prime/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a3857b969d7a7fed3cc3",
+    "url": "/test_prime/static/js/2.47682b93.chunk.js"
+  },
+  {
+    "revision": "8267d49f908edf88bb5c",
+    "url": "/test_prime/static/js/main.c8434a51.chunk.js"
+  },
+  {
+    "revision": "22fc506b98579b020e9a",
+    "url": "/test_prime/static/js/runtime-main.0c37a681.js"
+  }
+]);
